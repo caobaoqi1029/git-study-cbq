3d27fd980e6223232f7de3a616b030dd9900722a
cbq-windows <2024cbq@gmail.com>
2024-03-13 18:45:04 +0800

    chore: Dockerfile

--- sb3-demo/src/main/java/jzxy/cbq/sb3demo/Sb3DemoApplication.java
+++ sb3-demo/src/main/java/jzxy/cbq/sb3demo/Sb3DemoApplication.java
@@ -3,6 +3,10 @@ package jzxy.cbq.sb3demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Slf4j
@@ -12,6 +16,15 @@ public class Sb3DemoApplication {
         SpringApplication.run(Sb3DemoApplication.class, args);
 
         log.info("Sb3DemoApplication started successfully.");
+    }
+
+    @RestController
+    @RequestMapping("api/v1/hello")
+    static class HelloController {
+        @GetMapping("/sayHello/{name}")
+        public String sayHello(@PathVariable("name") String name) {
+            return "Hello I am " + name;
+        }
     }
 
 }
